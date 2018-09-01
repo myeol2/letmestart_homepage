2605ab32f522901477a0688a51d597aeba4e05e4
mgson8 <myeol2@snu.ac.kr>
2018-09-01 17:52:52 +0900

    letmestart page first commit, after embed facebookvideo

---
+++ HTMLCSS/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+
+    return render(request, 'HTMLCSS/index.html')
+
+def team(request):
+
+    return render(request, 'HTMLCSS/team.html')
+
+
+
+
