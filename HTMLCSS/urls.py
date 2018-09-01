7ac07a5af7ebe710ad8054815e89fd758e5f1a69
mgson8 <myeol2@snu.ac.kr>
2018-09-01 18:43:04 +0900

    name change in header

--- HTMLCSS/urls.py
+++ HTMLCSS/urls.py
@@ -5,5 +5,9 @@ from . import views
 urlpatterns = [
         path('index', views.index, name='index'),
         path('team', views.team, name='team'),
+        path('activity', views.activity, name='activity'),
+        path('recruit', views.recruit, name='recruit'),
+        path('contact', views.contact, name='contact'),
+        
         ]
 
--- HTMLCSS/views.py
+++ HTMLCSS/views.py
@@ -6,10 +6,22 @@ def index(request):
 
     return render(request, 'HTMLCSS/index.html')
 
+def activity(request):
+
+    return render(request, 'HTMLCSS/activity.html')
+
+
 def team(request):
 
     return render(request, 'HTMLCSS/team.html')
 
+def recruit(request):
+
+    return render(request, 'HTMLCSS/recruit.html')
+
+def contact(request):
+
+    return render(request, 'HTMLCSS/contact.html')
 
 
 
