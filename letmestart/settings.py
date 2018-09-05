4d5995e7f98eb05c30728f070c895062ce4fa4d2
mgson8 <myeol2@snu.ac.kr>
2018-09-06 00:55:35 +0900

    added allowed_host

--- letmestart/settings.py
+++ letmestart/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'w)!0irug#)e0m&710g4odw+a%%o$f3a0^$((&w($^c4huu(_qs'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+        '52.79.239.40',
+        'localhost',
+        ]
 
 
 # Application definition
