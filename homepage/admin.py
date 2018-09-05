de5e36214e7d1f2e41ac34f6c4356828aa410c85
mgson8 <myeol2@snu.ac.kr>
2018-09-05 16:52:29 +0900

    complete 'play' model

--- homepage/admin.py
+++ homepage/admin.py
@@ -2,14 +2,24 @@ from django.contrib import admin
 
 from .models import Play
 from .models import PlayImage
+from .models import PlayCasting
+from .models import PlayVideo
 # Register your models here.
+
+class PlayCastingInline(admin.TabularInline):
+    model = PlayCasting
+    extra = 3
 
 class PlayImageInline(admin.TabularInline):
     model = PlayImage
     extra = 6
 
+class PlayVideoInline(admin.TabularInline):
+    model = PlayVideo
+    extra = 3
+
 class PlayAdmin(admin.ModelAdmin):
-    inlines = [PlayImageInline,]
+    inlines = [PlayCastingInline, PlayImageInline, PlayVideoInline,]
 
 admin.site.register(Play, PlayAdmin)
 
--- homepage/models.py
+++ homepage/models.py
@@ -1,16 +1,46 @@
 from django.db import models
+from django.core import validators
 
 # Create your models here.
 
 class Play(models.Model):
-    title = models.CharField(max_length=50)
-    poster = models.ImageField(upload_to="homepage/static/img")
-    synopsis = models.TextField()
+    idx = models.PositiveIntegerField(
+            verbose_name='정기공연 회차',
+            default=1,
+            validators=[validators.MaxValueValidator(100), validators.MinValueValidator(1)],
+            )
+    title = models.CharField(
+            verbose_name='공연 제목',
+            max_length=50
+            )
+    poster = models.ImageField(
+            verbose_name='포스터 이미지',
+            upload_to="homepage/static/img")
+    synopsis = models.TextField(
+            verbose_name='시놉시스',
+            )
+    num_of_audience = models.PositiveIntegerField(
+            verbose_name='관객 수',
+            default=1,
+            )
 
+class PlayCasting(models.Model):
+    play = models.ForeignKey(Play, related_name='castings', on_delete=models.CASCADE)
+    casting = models.ImageField(
+            verbose_name='배역 이미지',
+            )
 
 class PlayImage(models.Model):
     play = models.ForeignKey(Play, related_name='images', on_delete=models.CASCADE)
-    image = models.ImageField()
+    image = models.ImageField(
+            verbose_name='공연 관련 사진'
+            )
+
+class PlayVideo(models.Model):
+    play = models.ForeignKey(Play, related_name='video_urls', on_delete=models.CASCADE)
+    video_url = models.CharField(
+            verbose_name='공연 관련 링크(극 공개, 티저, 인터뷰순)',
+            max_length=200)
 
 
 
