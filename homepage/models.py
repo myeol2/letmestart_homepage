73fd56cf4265c770a619b87d2e9a1dc9fec2cee5
mgson8 <myeol2@snu.ac.kr>
2018-09-09 15:00:30 +0900

    PlayMemeber &  PlayPhoto model, groupphoto field in Play

--- homepage/models.py
+++ homepage/models.py
@@ -28,7 +28,8 @@ class Play(models.Model):
             )
     poster = models.ImageField(
             verbose_name='포스터 이미지',
-            upload_to=play_poster_path,)
+            upload_to=play_poster_path,
+            )
     synopsis = models.TextField(
             verbose_name='시놉시스',
             )
@@ -36,10 +37,80 @@ class Play(models.Model):
             verbose_name='관객 수',
             default=1,
             )
+    group_photo = models.ImageField(
+            verbose_name='단체사진',
+            upload_to=play_poster_path,
+            )
 
     def __str__(self):
         return ('제 %d회 정기공연 : %s' % (self.idx, self.title))
 
+class PlayMember(models.Model):
+    play = models.ForeignKey(Play, related_name="members", on_delete=models.CASCADE)
+    
+    PLANNING = u"기획"
+    STAGE = u"무대"
+    ACT = u"배우"
+    DIRECT = u"연출"
+    MUSIC = u"음악"
+    DANCE = u"안무"
+    
+    LEADER = u'공동대표'
+    PRESIDENT = u'회장'
+    VICE_PRESIDENT = u'부회장'
+    TEAM_LEADER = u'팀장'
+    TEAM_MEMBER = u'팀원'
+
+    TEAM_CHOICES=(
+            (PLANNING, u'기획'),
+            (STAGE, u'무대'),
+            (ACT, u'배우'),
+            (DIRECT, u'연출'),
+            (MUSIC, u'음악'),
+            (DANCE, u'안무(9기 이전)'),
+            )
+
+    POSITION_CHOICES=(
+            (LEADER, u'공동대표'),
+            (PRESIDENT, u'회장'),
+            (VICE_PRESIDENT, u'부회장'),
+            (TEAM_LEADER, u'팀장'),
+            (TEAM_MEMBER, u'팀원'),
+          )
+
+    name = models.CharField(
+                verbose_name=u'이름',
+                max_length=10, 
+            )
+    team = models.CharField(
+            verbose_name=u'팀',
+            max_length=2,
+            choices = TEAM_CHOICES,
+            )
+
+    
+    position = models.CharField(
+            choices = POSITION_CHOICES,
+            max_length=6,
+            default = TEAM_MEMBER 
+            )
+    
+
+    admission_year = models.IntegerField(
+                verbose_name=u"학번",
+                validators=[validators.MinValueValidator(0), validators.MaxValueValidator(99)]
+                
+                )
+
+    major = models.CharField(
+                verbose_name=u"학과",
+                max_length=10,
+            )
+
+    admission_order_letme = models.IntegerField(
+                verbose_name=u'기수',
+                validators=[validators.MinValueValidator(0), validators.MaxValueValidator(100)]
+                )
 
 class PlayCasting(models.Model):
     play = models.ForeignKey(Play, related_name='castings', on_delete=models.CASCADE)
@@ -52,7 +123,14 @@ class PlayImage(models.Model):
     play = models.ForeignKey(Play, related_name='images', on_delete=models.CASCADE)
     image = models.ImageField(
             upload_to=play_media_path,
-            verbose_name='공연 관련 사진'
+            verbose_name='공연 사진'
+            )
+
+class PlayPhoto(models.Model):
+    play = models.ForeignKEy(Play, related_name='photos', on_delete=models.CASCADE)
+    photo = models.ImageField(
+            upload_to=play_media_path,
+            verbose_name='공연 준비 사진'
             )
 
 class PlayVideo(models.Model):
